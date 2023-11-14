549e4d8419bec1fc9775b5bd101c856b38fe08f6
nopnop <nopnop9090@v40.net>
2023-11-14 08:53:25 +0100

    Update Teilnehmer.java

--- src/main/java/Teilnehmer.java
+++ src/main/java/Teilnehmer.java
@@ -1,4 +1,62 @@
 
 public class Teilnehmer {
 
+	// Attribute
+	private String tnr;
+	private String gruppe;
+	private String name;
+	private String vorname;
+	
+	// Konstruktor
+	public Teilnehmer(String tnr, String gruppe, String name, String vorname) {
+		super();
+		this.tnr = tnr;
+		this.gruppe = gruppe;
+		this.name = name;
+		this.vorname = vorname;
+	}
+
+	// GETTER & SETTER
+	public String getTnr() {
+		return tnr;
+	}
+
+	public void setTnr(String tnr) {
+		this.tnr = tnr;
+	}
+
+	public String getGruppe() {
+		return gruppe;
+	}
+
+	public void setGruppe(String gruppe) {
+		this.gruppe = gruppe;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	// Zur Anzeige in der Liste
+	@Override
+	public String toString() {
+		return tnr + " - " + name + ", " + vorname;
+	}
+	
+	// Zum Eintragen in die CSV 1;022023;Muster;Max
+	public String toCSV() {
+		return tnr + ";" + gruppe + ";" + name + ";" + vorname;
+	}
 }
